a35df7cf587fa1b236744f6b87e0e3e5f067413e
Olli Helenius <liff@iki.fi>
2025-02-17 10:09:40 +0200

    Deal with TRAIN-CASE with caps word

--- keyboards/crkbd/keymaps/liff/keymap.c
+++ keyboards/crkbd/keymaps/liff/keymap.c
@@ -166,3 +166,22 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, 1, 2, 3);
     return state;
 }
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case KC_MINS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
